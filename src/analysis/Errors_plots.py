5e853cc5ca5cf71c22ff4904949a5b435695235b
Laura Malkhasyan <malkhlaura@gmail.com>
2020-03-09 03:54:55 +0100

    Update plots.

--- src/analysis/Errors_plots.py
+++ src/analysis/Errors_plots.py
@@ -14,71 +14,69 @@ from src.model_code.levy import grad_levy
 from src.model_code.ackley import ackley
 from src.model_code.ackley import grad_ackley
 from scipy.optimize._numdiff import approx_derivative
+from bld.project_paths import project_paths_join as ppj
 
 
 
-def calculate_error(params, diff, func, grad):
-    method = ["center", "forward", "backward"]
-    error = {}
-    for i,m in enumerate(method):
-        diff_dict = {'diff': diff}
-        num_der = first_derivative(func, params,func_kwargs=diff_dict,
-                      method=m)
-        analytical_der = grad(params, diff)
-        error[m] = (num_der - analytical_der )/np.abs(analytical_der).clip(1e-8, np.inf)
-        #error[m] = first_derivative(func, params,func_kwargs=diff_dict,
-        #              method=m) - grad(params, diff)
-    return error
-calculate_error(np.array([1,2]), np.array([1,2,3]), ackley, grad_ackley)
-#k = int(len(diff))
-#diff_vec = np.zeros_like(diff)
-#for j in range(k):
-#    diff_vec[j] = np.linspace(0, 40, 100)
-#return np.array([diff_vec])
+if __name__ == "__main__":
+    def calculate_error(params, diff, func, grad):
+        """Difference between numerical and analytical derivatives, devided by
+            analytial derivative.
 
-diff_vec = np.linspace(0, 40, 100)
+        Args:
+            params(np.array): 1d numpy array of function arguments
+            diff(np.array): difficulty parameter, controls wiggliness of the function
+            func(np.array): functions for which derivatives are calculated
+            grad(np.array): gradients of the functions
 
-functions = [rastrigin, func_levy, ackley]
-gradients = [grad_rastrigin, grad_levy, grad_ackley]
-function_names = ["Rastrigin", "Levy"]
-for f,g, func_name in zip(functions, gradients, function_names):
-    error_vec = [calculate_error(np.array([5.0, 8.5]),diff_i, f, g) for
-        diff_i in diff_vec]
+        Returns:
+            error(np.array): numpy array of relative errors, calculated for different
+                methods
 
-    to_concat = []
-    for err, d in zip(error_vec, diff_vec):
-        df = pd.DataFrame(err)
-        df["difficulty"] = d
-        to_concat.append(df)
+        """
+        method = ["center", "forward", "backward"]
+        error = {}
+        for i,m in enumerate(method):
+            diff_dict = {'diff': diff}
+            num_der = first_derivative(func, params,func_kwargs=diff_dict,
+                          method=m)
+            analytical_der = grad(params, diff)
+            error[m] = (num_der - analytical_der )/np.abs(analytical_der).clip(1e-8, np.inf)
 
-    data = pd.concat(to_concat)
+        return error
 
-    data["param_name"] = "x_" + data.index.astype(str)
+    diff_vec = np.linspace(0, 40, 100)
 
+    functions = [rastrigin, func_levy]
+    gradients = [grad_rastrigin, grad_levy]
+    function_names = ["Rastrigin", "Levy"]
+    for f,g, func_name in zip(functions, gradients, function_names):
 
-    params= data.param_name.unique().tolist()
-    methods = ["center", "forward", "backward"]
-    fig, axes = plt.subplots(nrows=len(params), figsize=(5, 3 * len(params)))
+        error_vec = [calculate_error(np.array([5.0, 8.5]),diff_i, f, g) for
+            diff_i in diff_vec]
 
-    for param, ax in zip(params, axes.flatten()):
-        df = data.query(f"param_name == '{param}'")
-        for method in methods:
-            sns.lineplot(ax=ax, y=df[method], x=df["difficulty"], label=method)
+        to_concat = []
+        for err, d in zip(error_vec, diff_vec):
+            df = pd.DataFrame(err)
+            df["difficulty"] = d
+            to_concat.append(df)
 
+        data = pd.concat(to_concat)
+
+        data["param_name"] = "x_" + (data.index+1).astype(str)
 
 
-    #for function_name in enumerate(function_names):
-    #    fig.savefig(function_name.png)
+        params= data.param_name.unique().tolist()
+        methods = ["center", "forward", "backward"]
+        fig, axes = plt.subplots(nrows=len(params), figsize=(5,  3* len(params)))
 
-#def save_fig(data, name, filename):
-#    fig = plt.figure()
-#    ax = fig.add_subplot(111)
-#    ax.hist(data, color="blue")
-#    ax.set_title(name)
-#    fig.tight_layout()
-#    fig.savefig("{}_{}.png".format(filename,name), format='png')
-#    plt.close(fig)
-
-#filename = 'output_graph_2'
-#for data, name in zip(list_of_data, list_of_names):
-#    save_hist(data, name, filename)
+        for param, ax in zip(params, axes.flatten()):
+            df = data.query(f"param_name == '{param}'")
+            for method in methods:
+                sns.lineplot(ax=ax, y=df[method], x=df["difficulty"], label=method)
+            ax.set(ylabel=r'$\frac{\partial f}{\partial '+param+'}$')
+        fig.suptitle('Error of ' +func_name+' function Gradient', y=1)
+        plt.tight_layout()
+        with open(ppj("OUT_FIGURES", func_name + ".png"), "wb") as j:
+            fig.savefig(j)
+    # plot errors ackley
